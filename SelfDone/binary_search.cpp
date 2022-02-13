dd92ba34bc5b69cf3ae886229d582723872f2d02
Saksham Srivastava <saksham16dec@gmail.com>
2022-02-14 01:19:54 +0530

    initial commit

--- SelfDone/binary_search.cpp
+++ SelfDone/binary_search.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 using namespace std;
+
+int Binary_search(int arr[], int size, int key)
+{
+    int start = 0, end = size - 1, mid;
+    while (start <= end)
+    {
+        mid = (end + start) / 2;
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+        else if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
 int main()
 {
     int size = 7;
-    int start = 0, end = size - 1, mid, key;
+    int key;
     int arr[size] = {1, 2, 5, 17, 87, 90, 94};
     // binary search
-    cout << "Enter the number to find" << endl;
-    cin >> key;
-    mid = (end + start) / 2;
-    while (start < end)
-    {
-        if (arr[mid] == key)
-            cout << "The index of key is " << mid << endl;
-        else if (key > mid)
-            mid = start;
-        else if (key < mid)
-            mid = end;
-        else
-            cout << "Not found" << endl;
-    }
+    int index = Binary_search(arr, size, 90);
+    cout << index;
     return 0;
 }
