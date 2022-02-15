3712c0cdfb82d93443ad436cd6fd8600204bc6c0
Saksham Srivastava <saksham16dec@gmail.com>
2022-02-15 19:31:57 +0530

    some documentaion added

--- SelfDone/firstlast_occ.cpp
+++ SelfDone/firstlast_occ.cpp
@@ -1,3 +1,5 @@
+// You have been given a sorted array/list ARR consisting of ‘N’ elements. You are also given an integer ‘K’.
+// Now, your task is to find the first and last occurrence of ‘K’ in ARR
 #include <iostream>
 using namespace std;
 
