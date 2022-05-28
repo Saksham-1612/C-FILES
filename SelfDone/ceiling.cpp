448f72f6861f234b085b1eaf90b1cfd86c2a33e7
Saksham <saksham16dec@gmail.com>
2022-05-29 02:23:05 +0530

    another approach

--- SelfDone/ceiling.cpp
+++ SelfDone/ceiling.cpp
@@ -23,6 +23,31 @@ int Ceiling(int arr[], int size, int key)
     }
     return start;
 }
+// another approach
+int Ceiling2(int arr[], int size, int key)
+{
+    {
+        int start = 0, end = size - 1, mid, ans = -1;
+        while (start <= end)
+        {
+            mid = (end / 2) + (start / 2);
+            if (arr[mid] == key)
+            {
+                return mid;
+            }
+            else if (key > arr[mid])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+}
 int main()
 {
     int size = 7;
@@ -30,7 +55,9 @@ int main()
     int arr[] = {2, 3, 5, 9, 14, 16, 18};
     // binary search
     int index = Ceiling(arr, size, 15);
+    int point = Ceiling2(arr, size, 15);
     cout << "Number is : " << arr[index] << endl;
     cout << index << endl;
+    cout << point << endl;
     return 0;
 }
